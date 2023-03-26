e67983578fbde5e07e6087f87a6bf5f9454e3ce4
infoblox323 <128623991+infoblox323@users.noreply.github.com>
2023-03-26 13:37:30 +0530

    Update useronboardingStylesheet.js

--- useronboardingStylesheet.js
+++ useronboardingStylesheet.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	loadResource();
 	loadConnectDetails();
 	loadERGDetails();
+	loadWelcomevideos();
 	
 	   $("a").click(function(){
 		   console.log('hyperlink clicked');
@@ -130,6 +131,78 @@ function loadConnectDetails() {
 	});
 
 }
+
+
+
+function loadWelcomevideos() {
+	var url = "https://infoblox323.github.io/jsonapi/WelcomeVideos.json";
+	$.ajax({
+		url: url,
+		type: "get",
+		dataType: 'text',
+		error: function(data) {
+			//debugger;
+			alert('err');
+		},
+		success: function(data) {
+			const obj = JSON.parse(data);
+			var len = obj.length;
+			if (obj) {
+				var txt = "";
+				if (len > 0) {
+					for (var i = 0; i < len; i++) {
+						
+							jQuery('<div>', {
+    								id: 'list-group-item'+i,
+								    class: 'd-flex align-items-center',
+								}).appendTo('#listwelcomevideos');
+									
+									jQuery('<div>', {
+	    								id: 'avatar'+i,
+									    class: 'flex-shrink-0 me-3',
+									}).appendTo('#list-group-item'+i);
+									
+										var img = $('<img />', { 
+												  id: 'Myid'+i,
+												  src: obj[i].Icon,
+												  alt: obj[i].Title
+												});
+												//img.appendTo($('#YourDiv'));
+										img.appendTo('#avatar'+i);
+						
+									jQuery('<div>', {
+	    								id: 'tileContent'+i,
+									    class: 'flex-grow-1',
+									}).appendTo('#list-group-item'+i);
+										
+										jQuery('<h5>', {
+	    								id: 'name'+i,
+									    class: 'mb-0',
+									    text: obj[i].Name 
+									}).appendTo('#tileContent'+i);
+									
+									jQuery('<h6>', {
+	    								id: 'title'+i,
+									    class: 'mb-0 text-muted',
+									    text: obj[i].Title
+									}).appendTo('#tileContent'+i);
+									
+										
+									jQuery('<a>', {
+	    								id: 'title'+i,
+									    class: 'mb-0',
+									    text: obj[i].Desc,
+									    href:obj[i].URL
+									}).appendTo('#tileContent'+i);
+					
+					}
+				}
+			}
+		}
+	});
+
+}
+
 
 function loadERGDetails() {
 	var url = "https://infoblox323.github.io/jsonapi/EmployeeResourceGroups.json";
